feff84fb32162fd91ac69090304dae3d7aadc110
ssamueluntu <yakusoku751@gmail.com>
2024-06-11 12:04:48 +0800

    style: ubah layout

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,2 +1,11 @@
+const profileMenu = document.querySelector(".profileddtrigger");
+const dropdownContainer = document.querySelector(".ddcontainer");
+
+profileMenu.addEventListener("click", function () {
+    dropdownContainer.classList.toggle("active");
+});
+
+
+
 const books = require("../../../data.json");
-console.log(books)
+console.log(books);
